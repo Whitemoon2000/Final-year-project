6dcc3dcae0747ff50c171f36d58090747dba5276
Whitemoon2000 <2431121i@student.gla.ac.uk>
2021-11-12 11:55:49 +0000

    Login and Register

---
+++ CSGame/Game/views.py
@@ -0,0 +1,38 @@
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login
+
+# Create your views here.
+def home(request):
+    return render(request, 'Home_page.html')
+
+def Signup(request):
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('Login')
+    else :
+        form = UserCreationForm()
+    return render(request, 'Signup_page.html',{'form':form})
+    
+
+def Login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'Login_page.html',{'form':form})
+
+def profile(request):
+    context = {
+        'user' : request.user 
+    }
+    return render(request, 'Profile_page.html', context)
